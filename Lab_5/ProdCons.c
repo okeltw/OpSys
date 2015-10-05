7e1a8820aeb040860c6d0990ebae6e2a3e6cb5ce
Taylor Okel <okeltw@mail.uc.edu>
2015-10-04 22:49:09 -0400

    Working.
    * Decided user space
    * Working on dynamic interface with streams

--- Lab_5/ProdCons.c
+++ Lab_5/ProdCons.c
@@ -1,6 +1,9 @@
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/kthread.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "queue_a.h"
 
 #define Q &((Stream*)stream)->buffer
 #define Lock &((Stream*)stream)->lock
@@ -26,3 +29,99 @@ typedef struct stream_struct {
 typedef struct {
    Stream *self, *prod;
 } Args;
+
+typedef struct {
+	int consumerID;
+	int tokenIndex;
+} ConnectEntry;
+
+typedef struct {
+	int nextToken;
+	ConnectEntry connectedConsumer;
+} connection;
+
+int consumerIDs[];
+int numConsumers;
+
+void IsEmpty(void);
+
+
+int get(int id){
+	return 0;
+}
+
+int menu(void){
+	int choice = 0;
+
+	printf("Choose an option:\n=================\n");
+	printf("1) Connect consumer\n2) Disconnect Consumer\n3) Consume\n4) Cancel Consume Request\n5) Check Buffers\n6) Quit\n>>> ");
+	
+	scanf("%i",&choice);
+
+	return choice;
+}
+
+void connect(void){
+	int id = 0;
+	bool flag = 1;
+	
+	while(flag){	
+		printf("Taken IDs:\n");
+		for(int i=0; i<numConsumers;i++){
+			printf("%i\n", consumerIDs[i]);
+		}
+		printf("=============\nAssign consumer id (integer):\n>>> ");
+		scanf("%i", &id);
+			
+		flag = 0;
+		for(int i=0; i<numConsumers;i++){
+			if(id == consumerIDs[i]){
+				printf("\n\nID taken. Choose a new ID.\n");
+				flag = 1;			
+			}
+		}
+		
+	}
+	
+	consumerIDs = realloc(consumerIDs, (numConsumers + 1)*sizeof(int));
+}
+
+int main(void){
+	int nav = 0;
+	numConsumers = 0;
+	consumerIDs = malloc(0*sizeof(int));
+
+	while(nav!=6){
+		nav = menu();
+		if(!(nav >= 0 && nav <= 6))
+		{	printf("Menu options limited to integers between 0 and 6.\n");	}
+
+		switch (nav){
+			case 0:
+				break;
+			case 1:
+				connect();
+				break;
+			case 2:
+				printf("\nDisconnect\n");
+				break;
+			case 3:
+				printf("\nConsume\n");
+				break;
+			case 4:
+				printf("\nCancel\n");
+				break;
+			case 5:
+				printf("\nCheck\n");
+				break;
+			case 6:
+				break;
+		}		
+
+
+
+	}
+	
+	return 0;
+}
+	
