fd6c11a2d9c397baa8f3a7da37db90bbb8c66fd4
Taylor Okel <okeltw@mail.uc.edu>
2015-11-01 02:45:05 -0500

    finished lab 6

--- Lab_3/.mult_threads.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/taylor/Documents/Labs/Lab_3/mult_threads.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/taylor/Documents/Labs/Lab_3/mult_threads.ko /home/taylor/Documents/Labs/Lab_3/mult_threads.o /home/taylor/Documents/Labs/Lab_3/mult_threads.mod.o
